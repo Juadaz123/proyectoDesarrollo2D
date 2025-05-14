ee787e8bcd83aa14016faa13bb8d69bbdab0570f
Juadaz123 <160087791+Juadaz123@users.noreply.github.com>
2025-05-14 14:18:23 -0500

    sprites de explosion, y asignación de mis tareas a realizar

--- RPGProyect/Assets/TAREAS.cs
+++ RPGProyect/Assets/TAREAS.cs
@@ -4,13 +4,18 @@
 LISTA DE TAREAS:
 
 Raizo
-1. Crear un script que permita transición entre niveles (escenas) cuando se interactúa con un objeto a modo de puerta. La siguiente escena será elegida de modo aleatorio entre una lista de posibles escenas.
+1. Crear un script que permita transiciï¿½n entre niveles (escenas) cuando se interactï¿½a con un objeto a modo de puerta. La siguiente escena serï¿½ elegida de modo aleatorio entre una lista de posibles escenas.
 2. Crear un script que permita al jugador recoger objetos (prefabs).
 3. Crear un script que permita al jugador almacenar monedas, llaves y pergaminos con un display en canvas.
-4. Crear un script que permita al jugador abrir puertas usando llaves del inventario (la puerta debe cambiar de sprite y colisión).
+4. Crear un script que permita al jugador abrir puertas usando llaves del inventario (la puerta debe cambiar de sprite y colisiï¿½n).
 5. Crear un script que permita al jugador usar monedas para abrir cofres (Cambiar el sprite del cofre al abrirlo) y que dropee monedas, llaves o pergaminos (3 de cualquiera de los objetos).
-4. Crear un script que permita al jugador usar pergaminos para crear explociones.
-5. Crear un script que permita a los pergaminos destruir objetos específicos en su área de impacto.
+4. Crear un script que permita al jugador usar pergaminos para crear explociones. 
+Juan
+5. Crear un script que permita a los pergaminos destruir objetos especï¿½ficos en su ï¿½rea de impacto.
+Juan
+7. Crear un sistema de Spawner de enemigos. 
+8. terminar de implementar un sistema de patrones de moviminento para enemigos
+Juan
 
 
 
