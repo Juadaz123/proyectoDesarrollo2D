c5dbeb2a3019828f6485198a68fd58bec806854a
Rhynder <89040329+RaizoValdivia@users.noreply.github.com>
2025-05-14 12:33:31 -0500

    Agregada lista de tareas

---
+++ RPGProyect/Assets/TAREAS.cs
@@ -0,0 +1,51 @@
+
+/*
+
+LISTA DE TAREAS:
+
+Raizo
+1. Crear un script que permita transición entre niveles (escenas) cuando se interactúa con un objeto a modo de puerta. La siguiente escena será elegida de modo aleatorio entre una lista de posibles escenas.
+2. Crear un script que permita al jugador recoger objetos (prefabs).
+3. Crear un script que permita al jugador almacenar monedas, llaves y pergaminos con un display en canvas.
+4. Crear un script que permita al jugador abrir puertas usando llaves del inventario (la puerta debe cambiar de sprite y colisión).
+5. Crear un script que permita al jugador usar monedas para abrir cofres (Cambiar el sprite del cofre al abrirlo) y que dropee monedas, llaves o pergaminos (3 de cualquiera de los objetos).
+4. Crear un script que permita al jugador usar pergaminos para crear explociones.
+5. Crear un script que permita a los pergaminos destruir objetos específicos en su área de impacto.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+using UnityEngine;
+public class TAREAS : MonoBehaviour
+
+{
+    void Start()
+    {   
+    }
+    void Update()
+    { 
+    }
+}
